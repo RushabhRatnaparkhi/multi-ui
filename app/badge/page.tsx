a0984a5f2bbf5e384bc487bc5f694f1cfb64a8f7
RushabhRatnaparkhi <rushuratnaparkhi2@gmail.com>
2025-06-28 13:35:26 +0530

    first safelist created

--- app/badge/page.tsx
+++ app/badge/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import BadgeExample1 from './examples/Example_1'
 import BadgeExample4 from './examples/Example_4'
+import BadgeExample2 from './examples/Example_2'
+import BadgeExample5 from './examples/Example_5'
+import BadgeExample3 from './examples/Example_3'
+import BadgeExample6 from './examples/Example_6'
+
+// This is a simple page that renders multiple badge examples
+// You can add more examples as needed
 
 const page = () => {
   return (
     <div>
       <BadgeExample1/>
       <BadgeExample4/>
+      <BadgeExample2/>
+      <BadgeExample5/>
+      <BadgeExample3/>
+      <BadgeExample6/>
     </div>
   )
 }
